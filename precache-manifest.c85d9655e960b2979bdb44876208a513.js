79dad4f8ee8156a79696adce52e599be0bebb633
Tyler Long <tyler4long@gmail.com>
2018-06-20 07:19:51 +0800

    Add new files

---
+++ precache-manifest.c85d9655e960b2979bdb44876208a513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4522a4a7f5961e957346d00126c0bd66",
+    "url": "oauth.html"
+  },
+  {
+    "revision": "5fb95b32d07f2e637afb1fa032ca9261",
+    "url": "index.html"
+  },
+  {
+    "url": "index.654b497d266269c7f48d.js"
+  },
+  {
+    "url": "index.654b497d266269c7f48d.css"
+  },
+  {
+    "revision": "c3ddd3aecb3300468f819e988b32d152",
+    "url": "favicon.ico"
+  },
+  {
+    "url": "e79bfd88537def476913f3ed52f4f4b3.eot"
+  },
+  {
+    "url": "a37b0c01c0baf1888ca812cc0508f6e2.ttf"
+  }
+];
