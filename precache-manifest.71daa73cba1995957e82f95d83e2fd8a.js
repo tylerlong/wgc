c60b1987e431e44772d6819f3e507fdd6f449a41
Tyler Long <tyler4long@gmail.com>
2018-06-19 21:16:23 +0800

    Initial commit

---
+++ precache-manifest.71daa73cba1995957e82f95d83e2fd8a.js
@@ -0,0 +1,20 @@
+self.__precacheManifest = [
+  {
+    "revision": "4522a4a7f5961e957346d00126c0bd66",
+    "url": "oauth.html"
+  },
+  {
+    "revision": "4c77cb824c4c18dfca3aa8f7eaf13432",
+    "url": "index.html"
+  },
+  {
+    "url": "index.c53384ca4280d272bcd5.js"
+  },
+  {
+    "url": "index.c53384ca4280d272bcd5.css"
+  },
+  {
+    "revision": "c3ddd3aecb3300468f819e988b32d152",
+    "url": "favicon.ico"
+  }
+];
