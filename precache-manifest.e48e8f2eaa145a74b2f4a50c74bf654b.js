5b5165e5f8a7070e7a9fabf9cea17cb010c060da
Tyler Long <tyler4long@gmail.com>
2018-07-04 23:22:45 +0800

    release

---
+++ precache-manifest.e48e8f2eaa145a74b2f4a50c74bf654b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "url": "012cf6a10129e2275d79d6adac7f3b02.woff"
+  },
+  {
+    "revision": "4522a4a7f5961e957346d00126c0bd66",
+    "url": "oauth.html"
+  },
+  {
+    "url": "570eb83859dc23dd0eec423a49e147fe.woff2"
+  },
+  {
+    "url": "index.292886c65fe573fc2750.css"
+  },
+  {
+    "revision": "16566372f17fd47abc3ee1f9cb7c75b3",
+    "url": "index.html"
+  },
+  {
+    "url": "index.292886c65fe573fc2750.js"
+  },
+  {
+    "url": "fcebde2dffaaa62bc14eb86e756081b1.png"
+  },
+  {
+    "revision": "c3ddd3aecb3300468f819e988b32d152",
+    "url": "favicon.ico"
+  },
+  {
+    "url": "e79bfd88537def476913f3ed52f4f4b3.eot"
+  },
+  {
+    "url": "c0087d4d5ddd32ec52f1859b42956db6.eot"
+  },
+  {
+    "url": "ae8767ca8ecf4b090af58eff5dd6d3a7.woff2"
+  },
+  {
+    "url": "a37b0c01c0baf1888ca812cc0508f6e2.ttf"
+  },
+  {
+    "url": "7d2560aebef60315bb781a52ff0c1773.png"
+  },
+  {
+    "url": "4348368ab857cb57b18e1a87ba8ff284.ttf"
+  },
+  {
+    "url": "2e9a0313ed02aed1e206340feb36ec5c.woff"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.f3ef54a3041fee640de4cbc85dedb5ca.js"
+  "precache-manifest.e48e8f2eaa145a74b2f4a50c74bf654b.js"
 );
 
 /**
