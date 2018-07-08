99679f512abc3544b3105f93c0f131f1c9b46c84
Tyler Long <tyler4long@gmail.com>
2018-07-08 20:45:18 +0800

    release

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.a2c39a54ae2599ee837bb0c9597cdef0.js"
+  "precache-manifest.83758f09500e49a528a538882b5709f3.js"
 );
 
 /**
